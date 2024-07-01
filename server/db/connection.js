ed5bec47019576bf034c530cc9720fd6c6dff17b
Deep Patel <deep1907rp@gmail.com>
2024-07-01 11:39:51 +0530

    nodemon watch env

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 export const connect = async () => {
 	return await mongoose.connect(process.env.CONNECTIONSTRING, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
+		// useNewUrlParser: true,
+		// useUnifiedTopology: true,
 		// useNewUrlParser: true,
 		// useUnifiedTopology: true,
 		// useMongoClient: true,
